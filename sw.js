55068fd122656eacc5bfd48c7ab0b51635725961
Narumi Masao <42761436+msonrm@users.noreply.github.com>
2025-09-19 15:55:49 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'xsa-v2.16.0';
+const CACHE_NAME = 'xsa-v2.17.0';
 const urlsToCache = [
   './',
   './index.html',
@@ -11,7 +11,7 @@ const urlsToCache = [
 
 // インストール時の処理
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Install v2.16.0');
+  console.log('Service Worker: Install v2.17.0');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
